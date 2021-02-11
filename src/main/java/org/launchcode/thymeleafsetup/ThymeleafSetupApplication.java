d19abfe7893fff21a9702a0baee2170267c0eeeb
farleysm8909 <farleysm8909@uwec.edu>
2021-02-11 15:01:06 -0600

    Initial commit

---
+++ src/main/java/org/launchcode/thymeleafsetup/ThymeleafSetupApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.thymeleafsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafSetupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/thymeleafsetup/ThymeleafSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.thymeleafsetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
